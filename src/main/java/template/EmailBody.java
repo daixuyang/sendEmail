a90676bc69b61873620bde579b8c45ec3d4f81b4
DaiXuYang <821531843@qq.com>
2018-07-30 15:51:14 +0800

    Initial commit

---
+++ src/main/java/template/EmailBody.java
@@ -0,0 +1,16 @@
+package template;
+
+/**
+ * 邮件正文
+ * @Author 小代[821531843@qq.com]
+ * @Date 2018/7/27 13:23
+ */
+public class EmailBody {
+
+    public static String body(){
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("hello");
+        return stringBuffer.toString();
+    }
+
+}
